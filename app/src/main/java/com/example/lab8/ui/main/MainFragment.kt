f1f2afd9a248b0aa73cd57994f7125b919a1bfaa
Grigory <lalaevgri@gmail.com>
2023-05-20 16:29:30 +0300

    add splash and smth

---
+++ app/src/main/java/com/example/lab8/ui/main/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.lab8.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lab8.R
+import com.example.lab8.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/lab8/ui/search/SearchFragment.kt
@@ -0,0 +1,24 @@
+package com.example.lab8.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.lab8.R
+import com.example.lab8.databinding.FragmentSearchBinding
+
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+}
