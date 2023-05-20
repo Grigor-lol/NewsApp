502cbd9bd58103096ded124595436c92e85086a8
Grigory <lalaevgri@gmail.com>
2023-05-20 16:49:44 +0300

    add retrofit

---
+++ app/src/main/java/com/example/lab8/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.lab8.utils
+
+class Constants {
+
+
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "3e9dc9dba4ee48ac96b4ac34bf912ca9"
+    }
+}
